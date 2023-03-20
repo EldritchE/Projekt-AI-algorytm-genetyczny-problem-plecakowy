b7664455bd20dba5ceb9468727fc15057d398dcf
EldritchE <kontakt@autoserwisjaworski.pl>
2023-03-20 15:42:47 +0100

    Gotowy bardziej

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import random
 import matplotlib.pyplot as plt
 import math
 from scipy import stats
+
 plt.close('all')
 
-    
 # Dane wejściowe
-wagaPrzedmiotu =    [12, 8, 15, 2, 9, 17, 36, 8, 14, 9]
+wagaPrzedmiotu = [12, 8, 15, 2, 9, 17, 36, 8, 14, 9]
 wartoscPrzedmiotu = [25, 32, 5, 8, 16, 12, 19, 2, 14, 3]
 pojemnoscPlecaka = 89
 
@@ -14,17 +14,19 @@ pojemnoscPlecaka = 89
 rozmiarPopulacji = 40
 wspolczynnikMutacji = 0.01
 generacjeIlosc = 20
-wspolczynnikKrzyzowania=0.8
+wspolczynnikKrzyzowania = 0.8
 
 
 def Generowanie_Indywidualne():
     return [random.randint(0, 1) for _ in range(len(wagaPrzedmiotu))]
 
+
 populacja = [Generowanie_Indywidualne() for _ in range(rozmiarPopulacji)]
+
 
 # oceniać kondycję poszczególnych dopasowan
 def OcenaDopasowan(jednostka):
-    liczbakrzyzowek=0
+    liczbakrzyzowek = 0
     wagaCalkowita = sum([jednostka[i] * wagaPrzedmiotu[i] for i in range(len(wagaPrzedmiotu))])
     wartoscCalkowita = sum([jednostka[i] * wartoscPrzedmiotu[i] for i in range(len(wartoscPrzedmiotu))])
     # print("+")
@@ -35,7 +37,7 @@ def Ruletka(population, wynikDopasowania):
     start = 0.0
     suma = sum(wynikDopasowania)
     los = random.random()
-    nowyStart=0.0
+    nowyStart = 0.0
     for x in range(len(wynikDopasowania)):
         nowyStart += wynikDopasowania[x] / suma
         if (start <= los < nowyStart):
@@ -58,40 +60,37 @@ def mutate(jednostka, szansa=0.1):
             jednostka[i] = 1 - jednostka[i]
 
 
-y=[]
-wszystkieSredniePopulacji=[]
+y = []
+wszystkieSredniePopulacji = []
 for generacja in range(generacjeIlosc):
 
-
     wynikDopasowania = [OcenaDopasowan(jednostka) for jednostka in populacja]
-    print("Wyniki dopasowania całej polpulacji ",wynikDopasowania)
+    print("Wyniki dopasowania całej polpulacji ", wynikDopasowania)
 
-
-    
-
-    kary=0
+    kary = 0
     for x in range(len(populacja)):
-        if(x < 9):
-            print("osobnik #",x+1,"  ",populacja[x],"Wynik dopasowania tego osobnika : ",wynikDopasowania[x],end="")
-            if wynikDopasowania[x]==0:
-                print(" Osobnik ",x+1,"jest przeładowany i ponosi karę",end="")
+        if (x < 9):
+            print("osobnik #", x + 1, "  ", populacja[x], "Wynik dopasowania tego osobnika : ", wynikDopasowania[x],
+                  end="")
+            if wynikDopasowania[x] == 0:
+                print(" Osobnik ", x + 1, "jest przeładowany i ponosi karę", end="")
                 kary += 1
             print("")
         else:
-            print("osobnik #",x+1," ",populacja[x],"Wynik dopasowania tego osobnika : ",wynikDopasowania[x],end="")
-            if wynikDopasowania[x]==0:
-                print(" Osobnik ",x+1,"jest przeładowany i ponosi karę",end="")
+            print("osobnik #", x + 1, " ", populacja[x], "Wynik dopasowania tego osobnika : ", wynikDopasowania[x],
+                  end="")
+            if wynikDopasowania[x] == 0:
+                print(" Osobnik ", x + 1, "jest przeładowany i ponosi karę", end="")
                 kary += 1
             print("")
     print("\n")
-    
 
     nowaPopulacja = []
-    odrzucone=0
+    odrzucone = 0
     for i in range(rozmiarPopulacji // 2):
-        rodzic1, rodzic2 = Ruletka(populacja,wynikDopasowania), Ruletka(populacja,wynikDopasowania)
+        rodzic1, rodzic2 = Ruletka(populacja, wynikDopasowania), Ruletka(populacja, wynikDopasowania)
         while rodzic2 == rodzic1:
-            rodzic2 = Ruletka(populacja,wynikDopasowania)
+            rodzic2 = Ruletka(populacja, wynikDopasowania)
         if random.uniform(0, 1) <= wspolczynnikKrzyzowania:
             dziecko1, dziecko2 = krzyzowanie(rodzic1, rodzic2)
         # Przechodzą dalej czy mamy losować nową parę na ich miejsce???
@@ -109,27 +108,25 @@ for generacja in range(generacjeIlosc):
     # wybór najlepszego osobnika
     najlepszy_osobnik = max(populacja, key=OcenaDopasowan)
 
-
     najlepszeDopasowanie = OcenaDopasowan(najlepszy_osobnik)
     y.append(najlepszeDopasowanie)
-# Wyniki
-    sredniaPopulacji=sum(wynikDopasowania)/rozmiarPopulacji
+    # Wyniki
+    sredniaPopulacji = sum(wynikDopasowania) / rozmiarPopulacji
     wszystkieSredniePopulacji.append(sredniaPopulacji)
-    print("Średnia wartości dopasowań populacji ",sredniaPopulacji)
+    print("Średnia wartości dopasowań populacji ", sredniaPopulacji)
     print("Najlepszy Osobnik:", najlepszy_osobnik)
     print("Najlepsze Dopasowanie Wartości:", najlepszeDopasowanie)
 
-
     najlepszaWaga = 0
-# print(najlepszy_osobnik)
-# print(wagaPrzedmiotu)
+    # print(najlepszy_osobnik)
+    # print(wagaPrzedmiotu)
     for x in range(len(wagaPrzedmiotu)):
-        if najlepszy_osobnik[x]==1:
-            najlepszaWaga= najlepszaWaga + wagaPrzedmiotu[x]
+        if najlepszy_osobnik[x] == 1:
+            najlepszaWaga = najlepszaWaga + wagaPrzedmiotu[x]
 
-    print ("Przy Wadze:", najlepszaWaga)
+    print("Przy Wadze:", najlepszaWaga)
     print("Ilość osobników w populacji ukaranych za przeładowanie: )", kary, "\n")
-    print("*"*70)
+    print("*" * 70)
 
     plt.plot(generacja + 1, najlepszeDopasowanie, 'ro')
     plt.plot(generacja + 1, sredniaPopulacji, 'bo')
@@ -137,35 +134,26 @@ for generacja in range(generacjeIlosc):
 
     plt.ylabel("Najlepsze dopadsowanie wartości")
     plt.xlabel("Kolejna generacja")
-    x = range(1,generacjeIlosc+1)
+    x = range(1, generacjeIlosc + 1)
     new_list = range(math.floor(min(x)), math.ceil(max(x)) + 1)
     plt.xticks(new_list)
     for x in range(len(populacja)):
-        if (OcenaDopasowan(populacja[x])==najlepszeDopasowanie):
-            plt.annotate("os"+str(x+1), (generacja+1, najlepszeDopasowanie))
+        if (OcenaDopasowan(populacja[x]) == najlepszeDopasowanie):
+            plt.annotate("os" + str(x + 1), (generacja + 1, najlepszeDopasowanie))
+
 x = range(1, generacjeIlosc + 1)
 slope, intercept, r, p, std_err = stats.linregress(x, y)
 
-def myfunc(x):
+def regresja(x):
     return slope * x + intercept
-
-mymodel = list(map(myfunc, x))
-
+mymodel = list(map(regresja, x))
 plt.scatter(x, y)
 plt.plot(x, mymodel)
-
 x = range(1, generacjeIlosc + 1)
-slope, intercept, r, p, std_err = stats.linregress(x,wszystkieSredniePopulacji)
+slope, intercept, r, p, std_err = stats.linregress(x, wszystkieSredniePopulacji)
 
-def myfunc(x):
-    return slope * x + intercept
 
-mymodel = list(map(myfunc, x))
-
+mymodel = list(map(regresja, x))
 plt.scatter(x, wszystkieSredniePopulacji)
 plt.plot(x, mymodel)
-
-
-
-
 plt.show()
